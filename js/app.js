7a608c02716c63ca886cf156ccf6eb6755a6aa9c
linhtein <yoteshin5599@gmail.com>
2021-12-25 10:45:49 +0630

    first

---
+++ js/app.js
@@ -0,0 +1,7 @@
+let currentPlace=document.querySelector('.current-place');
+let allPath=document.querySelectorAll('.map path');
+allPath.forEach(x=>{
+    x.addEventListener('mouseover',_=>{
+        currentPlace.innerText=x.getAttribute('title');
+    });
+})
